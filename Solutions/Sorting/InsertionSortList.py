16d966c602e837361a5207edfc821e8594538a2a
Aidana Baglanova <89396938+LilBaga@users.noreply.github.com>
2022-10-18 11:55:17 +0600

    Create InsertionSortList.py

---
+++ Solutions/Sorting/InsertionSortList.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp = []
+        node = head
+        while node:
+            temp += [node.val]
+            node = node.next
+        temp.sort()
+        res = root = ListNode(0)
+        for i in temp:
+            t  = ListNode(i)
+            root.next = t
+            root  = root.next
+        return res.next
